73f0336fdb8a617e0ed9cb734ce8ec394572ad44
Steven Johnson <sjohnson21960@gmail.com>
2020-08-14 15:09:59 -0500

    PATH UPDATE

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route path="/" component={About} />
+         <About/>
         </Wrapper>
         <Footer />
       </div>
@@ -20,3 +20,6 @@ function App() {
 }
 
 export default App;
+
+
+{/* <Route path="/" component={About} /> */}
