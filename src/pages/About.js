cb8f5a26879eaeeee637f106ee37add8b8a4cc64
Steven Johnson <sjohnson21960@gmail.com>
2020-08-14 14:56:13 -0500

    Larkin fixed my stuff

--- src/pages/About.js
+++ src/pages/About.js
@@ -23,6 +23,7 @@ class About extends Component {
   handleFormSubmit = event => {
     const { firstName, lastName, email, number } = this.state
     event.preventDefault();
+    console.log(number)
     if (firstName.trim().length === 0 || lastName.trim().length === 0 || email.trim().length === 0 || number.trim().length === 0) {
       alert('Fill out all of the inputs please');
       this.setState({
@@ -34,8 +35,9 @@ class About extends Component {
       return;
     } else {
       //Code to push new employee state into JSON file
-      let newEmployee = [{ firstName, lastName, email, number }]
-      this.setState({ employees: [...this.state.members, newEmployee] })
+      let newEmployee = { firstName, lastName, email, number }
+      console.log(newEmployee)
+      this.setState({ employees: [...this.state.employees, newEmployee] })
       console.log(employees);
       this.setState({
         firstName: "",
@@ -46,26 +48,26 @@ class About extends Component {
     };
   };
 
-  // renderList = () => {
-  //   if (0 === 0
-  //     // alphabetical
-  //   ) {
-  //     // Display employess by last name alphabetically
-  //   } else if (0 === 0
-  //     // email
-  //   ) {
-  //     // Display employees by email alphabetically
-  //   } else {
-  //     return (this.state.employees.map(employee => (
-  //       <FriendCard
-  //         firstName={employee.firstName}
-  //         lastName={employee.lastName}
-  //         email={employee.email}
-  //         number={employee.number}
-  //       />
-  //     )));
-  //   }
-  // }
+  renderList = () => {
+    if (0 === 1
+      // alphabetical
+    ) {
+      // Display employess by last name alphabetically
+    } else if (0 === 1
+      // email
+    ) {
+      // Display employees by email alphabetically
+    } else {
+      return (this.state.employees.map(employee => (
+        <FriendCard
+          firstName={employee.firstName}
+          lastName={employee.lastName}
+          email={employee.email}
+          number={employee.number}
+        />
+      )));
+    }
+  }
 
   render() {
     return (
@@ -83,28 +85,28 @@ class About extends Component {
               <h3>New Employee</h3>
               <form className="form">
                 <input
-                  value={this.state.employees.firstName}
+                  value={this.state.firstName}
                   name="firstName"
                   onChange={this.handleInputChange}
                   type="text"
                   placeholder="First Name"
                 />
                 <input
-                  value={this.state.employees.lastName}
+                  value={this.state.lastName}
                   name="lastName"
                   onChange={this.handleInputChange}
                   type="text"
                   placeholder="Last Name"
                 />
                 <input
-                  value={this.state.employees.email}
+                  value={this.state.email}
                   name="email"
                   onChange={this.handleInputChange}
                   type="text"
                   placeholder="Email"
                 />
                 <input
-                  value={this.state.employees.number}
+                  value={this.state.number}
                   name="number"
                   onChange={this.handleInputChange}
                   type="number"
@@ -117,15 +119,8 @@ class About extends Component {
           <Row>
             <Col size="md-12">
               <h3>Employee List</h3>
-              {/* {this.renderList()} */}
-              {this.state.employees.map(employee => (
-                <FriendCard
-                  firstName={employee.firstName}
-                  lastName={employee.lastName}
-                  email={employee.email}
-                  number={employee.number}
-                />
-              ))}
+              {this.renderList()}
+              
             </Col>
           </Row>
         </Container>
