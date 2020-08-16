68dd69b393503710ac374e10c291c5476d7dea82
Steven Johnson <sjohnson21960@gmail.com>
2020-08-16 12:09:29 -0500

    route path update

---
+++ build/precache-manifest.47d4aecf0d75f74ff749e1e596ba8867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afda27f3c1a8e8950915619f066b5cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aa9e54a651cd4bfafa5",
+    "url": "/static/css/main.a7b2b78d.chunk.css"
+  },
+  {
+    "revision": "ca2e04a503b334e6b0d0",
+    "url": "/static/js/2.98f5d08f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.98f5d08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa9e54a651cd4bfafa5",
+    "url": "/static/js/main.fe2fb963.chunk.js"
+  },
+  {
+    "revision": "26d5b5fc4d28fa5e258e",
+    "url": "/static/js/runtime-main.b1827233.js"
+  }
+]);
